2b22c096ae8cdbc0ea0ec3733b570c9e23fd1120
Leosky <kheless@gmail.com>
2017-11-05 22:00:11 +0100

    splitter fix

--- objects/smartbox/splitter/splitter.lua
+++ objects/smartbox/splitter/splitter.lua
@@ -7,21 +7,24 @@ function init()
 end
 
 function update(dt)
-	deltaTime = deltaTime + dt
+	q = deltaTime + dt
 	if deltaTime > 1 then
 		deltaTime = 0
 		transferUtil.loadSelfContainer()
 	end
-	refresh(100, 1)
-	refresh(10, 2)
-	refresh(1, 3)
+
+	 --test on output slots to avoid filling more than once
+	local id = entity.id()
+	if not world.containerItemAt(id, 1) then refresh(100, 1) end
+	if not world.containerItemAt(id, 2) then refresh(10, 2) end
+	if not world.containerItemAt(id, 3) then refresh(1, 3) end
 end
 
 function refresh(size, slot)
 	local id = entity.id()
 	local input = world.containerItemAt(id, 0)
 	if input then
-		local stack = world.containerTakeNumItemsAt(id, 0, (input.count // size) * size)
+		local stack = world.containerTakeNumItemsAt(id, 0, math.min(1, (input.count // size)) * size) -- only one stack
 		if stack then 
 			stack = world.containerPutItemsAt(id, stack, slot)
 			if stack then
@@ -34,3 +37,5 @@ function refresh(size, slot)
 		end
 	end
 end
+
+--Note : shift-clicking will fill already filled slots before slot 1. Idk if its possible to fix this inadabted behaviore for this block.
